11698df8d7bbd837f621e5184bae207edce1e3e4
Jerald Joseph <pingjerald@gmail.com>
2022-05-07 20:58:10 +0530

    cleanup, app.js index.css modification, added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4JaGhRK0Fcuc7H-qkmtawWeIyW4YAIFU",
+    authDomain: "clone-app-55d61.firebaseapp.com",
+    projectId: "clone-app-55d61",
+    storageBucket: "clone-app-55d61.appspot.com",
+    messagingSenderId: "117894843595",
+    appId: "1:117894843595:web:dbe2a0c3046bdce59a9f4e"
+  };

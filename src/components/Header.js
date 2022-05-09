969d39d2e0525951c4ba68cbdb63495cc92dc570
Jerald Joseph <pingjerald@gmail.com>
2022-05-09 15:34:45 +0530

    link tag added to amazon logo and shopping basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,20 @@ import "./Header.css" // 3.2
 import SearchIcon from '@mui/icons-material/Search'; // 3.3
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket'; // 3.3
 
+import {Link} from "react-router-dom" // 3.9
+
 function 
 Header () {
   return (
     <div className='header'>
         <div className='header__logoContainer'>
+
+        <Link to = "/"> {/* 3.9 */}
+
         <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" className='header__logo' />
+        
+        </Link>
+         
         </div>
 
         <div className='header__searchContainer'> 
@@ -33,10 +41,16 @@ Header () {
                 <span className='header__optionLineTwo'>Prime</span>
             </div>
 
+
+            <Link to = "/checkout" > {/* 3.9 */}
+            
             <div className='header__optionBasket'>
                 <ShoppingBasketIcon/> {/* 3.3 */}
                 <span className='header__optionLineTwo header__basketCount'>0</span> {/* 3.3 */}
             </div>
+
+            </Link>
+
 
         </div>
         
